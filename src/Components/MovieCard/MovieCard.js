34d0b544726c68e9a84d7c38a43e7582aff05f5a
Natalia Zamboni Vergara <nzamboniv@gmail.com>
2021-10-17 11:19:13 -0600

    Change MovieCard styling

--- src/Components/MovieCard/MovieCard.js
+++ src/Components/MovieCard/MovieCard.js
@@ -5,8 +5,10 @@ const MovieCard = ({ poster, title, ratings, id, selectMovie }) => {
   return (
     <article className="movie-card" id={id} onClick={() => selectMovie(id)}>
       <img className="card-poster" src={poster} alt={`${title} poster`} />
-      <h3 className="card-title">{title}</h3>
-      <p className="card-rating">⭐️ {Math.round(ratings * 10) / 10}</p>
+      <div className="card-details">
+        <h3 className="card-title">{title}</h3>
+        <p className="card-rating">⭐️{Math.round(ratings * 10) / 10}</p>
+      </div>
     </article>
   );
 };

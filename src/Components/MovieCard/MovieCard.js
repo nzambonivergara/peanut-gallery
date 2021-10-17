8ee02b278b23923c108a20a9d05ee0120247614a
Natalia Zamboni Vergara <nzamboniv@gmail.com>
2021-10-17 09:09:00 -0600

    Remove commented out code and add class names

--- src/Components/MovieCard/MovieCard.js
+++ src/Components/MovieCard/MovieCard.js
@@ -3,10 +3,10 @@ import './MovieCard.css';
 
 const MovieCard = ({ poster, title, ratings, id, selectMovie }) => {
   return (
-    <article id={id} onClick={() => selectMovie(id)}>
-      <img src={poster} alt={`${title} poster`} />
+    <article className="movie-card" id={id} onClick={() => selectMovie(id)}>
+      <img className="card-poster" src={poster} alt={`${title} poster`} />
       <h3 className="card-title">{title}</h3>
-      <p className="card-rating">Rating: {Math.round(ratings * 10) / 10}</p>
+      <p className="card-rating">⭐️ {Math.round(ratings * 10) / 10}</p>
     </article>
   );
 };

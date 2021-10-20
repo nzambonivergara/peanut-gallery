9178b09eef536dcd6fa6900424545ba76b47bce8
Raquel Hill <raquelturing@gmail.com>
2021-10-20 12:31:15 -0600

    implemented feedback, deleted console.logs, banner image now updates every time the page changes

--- src/Components/MovieCard/MovieCard.js
+++ src/Components/MovieCard/MovieCard.js
@@ -2,19 +2,11 @@ import React from 'react';
 import './MovieCard.css';
 import { Link } from 'react-router-dom';
 
-const MovieCard = ({ poster, title, ratings, id, selectMovie }) => {
+const MovieCard = ({ poster, title, ratings, id }) => {
   const rating = Math.round(ratings * 10) / 10;
   return (
     <Link to={`/${id}`}>
-      <article
-        className="movie-card"
-        // tabIndex="0"
-        id={id}
-        // onKeyPress={(event) => {
-        //   (event.key === 'Enter' || 'Space character') && selectMovie(id);
-        // }}
-        // onClick={() => selectMovie(id)}
-      >
+      <article className="movie-card" id={id}>
         <img className="card-poster" src={poster} alt={`${title} poster`} />
         <div className="card-details">
           <h2 className="card-title">{title}</h2>

ee1047ada5ec0496e006daea7657633305638b2d
Raquel Hill <raquelturing@gmail.com>
2021-10-24 17:45:29 -0600

    Begin styling for the search input field

--- src/Components/SearchForm/SearchForm.js
+++ src/Components/SearchForm/SearchForm.js
@@ -3,31 +3,32 @@ import './SearchForm.css';
 
 class SearchForm extends Component {
   constructor() {
-    super()
+    super();
     this.state = {
-      searchTerm: ''
-    }
+      searchTerm: '',
+    };
   }
 
   handleChange = (event) => {
-    this.setState({ [event.target.name]: event.target.value })
-    const searchTerm = event.target.value.toLowerCase().trim()
-    this.props.filterMovies(searchTerm)
-  }
+    this.setState({ [event.target.name]: event.target.value });
+    const searchTerm = event.target.value.toLowerCase().trim();
+    this.props.filterMovies(searchTerm);
+  };
 
   render() {
     return (
       <form>
         <input
+          className="search-input"
           type="text"
-          placeholder="Search movies by title..."
+          placeholder="🔎 Search movies by title..."
           name="searchTerm"
           value={this.state.searchTerm}
           onChange={(event) => this.handleChange(event)}
         />
       </form>
-    )
+    );
   }
 }
 
-export default SearchForm
+export default SearchForm;

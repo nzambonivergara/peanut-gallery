ea9dce9e248f8e912a6fff34ce34533a83578e00
Raquel Hill <raquelturing@gmail.com>
2021-10-24 17:43:59 -0600

    Begin styling for the search form.  Merge with login component

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -9,15 +9,18 @@ const Header = ({ bannerImage, clearFilteredMovies }) => {
         <div className="overlay"></div>
         <img className="banner" src={bannerImage} alt="movie backdrop" />
       </div>
-
       <div className="title-container">
         <h1 className="title">
           The <span>P🍿pcorn</span> Gallery
         </h1>
       </div>
-      <NavLink exact to="/" className="all-movies-nav">
       <h1 className="title">🍿 The Popcorn Gallery</h1>
-      <NavLink exact to="/" className="all-movies-nav" onClick={() => clearFilteredMovies()}>
+      <NavLink
+        exact
+        to="/"
+        className="all-movies-nav"
+        onClick={() => clearFilteredMovies()}
+      >
         ALL MOVIES
       </NavLink>
       <NavLink exact to="/signin" className="sign-in-nav">
